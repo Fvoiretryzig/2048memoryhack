d470cdbca4b7dd2cc5dfa8a992098f32637837bb
tracer <tracer@tracer.org>
2018-05-20 22:02:11 +0800

    >  compile forty Linux forty-virtual-machine 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:03 UTC 2018 i686 i686 i686 GNU/Linux

--- memhack.c
+++ memhack.c
@@ -115,7 +115,9 @@ void init(char* pid_c)
 	        	break;
 	        }     
 	        else{	//应该只会有一个数据段吧
-	        	if(!regexec(&data_seg,f_line,1,pm_data_seg,0)){printf("\033[44;33mpid:%d\033[0m\n",pid);
+	        	int ret = regexec(&data_seg,f_line,1,pm_data_seg,0);
+	        	printf("\033[44;33mret:%d\033[0m\n", ret);
+	        	if(!ret){printf("\033[44;33mpid:%d\033[0m\n",pid);
 					char* start = NULL; char* end = NULL;
 					int point = 0;
 					for(point = pm_data_seg[0].rm_so; point<pm_data_seg[0].rm_eo; point++){

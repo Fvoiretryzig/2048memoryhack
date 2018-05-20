9fe666596108ff390115917a1f1c440cba25e89a
tracer <tracer@tracer.org>
2018-05-20 22:15:17 +0800

    >  compile forty Linux forty-virtual-machine 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:03 UTC 2018 i686 i686 i686 GNU/Linux

--- memhack.c
+++ memhack.c
@@ -97,7 +97,7 @@ void init(char* pid_c)
 	regex_t data_seg;	
 	char* pattern_data_seg = "[0-9,a-d]{8}-[0-9,a-d]{8} rw-p";
 	/*int p_data_seg =*/ regcomp(&data_seg, pattern_data_seg, REG_EXTENDED);
-	regmatch_t pm_data_seg[5];
+	regmatch_t pm_data_seg[1];
 	regex_t stop;
 	char* pattern_stop = "[h,e,a,p]{4}]";
 	/*int p_stop =*/ regcomp(&stop, pattern_stop, REG_EXTENDED);
@@ -109,15 +109,15 @@ void init(char* pid_c)
     	
         if(fgets(f_line, 1024,fp)){
 	        printf("%s", f_line);  
-	        f_line[strlen(f_line)+1] = NULL;
-	        printf("%s", f_line);  
+	        //f_line[strlen(f_line)+1] = NULL;
+	        //printf("%s", f_line);  
 	        int ret = regexec(&stop,f_line,1,pm_stop,0);
 	        	printf("\033[44;33mret:%d\033[0m\n", ret);
 	        if(!ret){
 	        	break;
 	        }     
 	        else{	//应该只会有一个数据段吧
-	        	int ret = regexec(&data_seg,f_line,5,pm_data_seg,0);
+	        	int ret = regexec(&data_seg,f_line,1,pm_data_seg,0);
 	        	printf("\033[44;33mret:%d\033[0m\n", ret);
 	        	if(!ret){printf("\033[44;33mpid:%d\033[0m\n",pid);
 					char* start = NULL; char* end = NULL;

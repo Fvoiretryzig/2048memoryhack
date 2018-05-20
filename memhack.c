1368e669082aea1f663d99c673ed2ee80f3e406a
tracer <tracer@tracer.org>
2018-05-20 21:25:22 +0800

    >  compile forty Linux forty-virtual-machine 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:03 UTC 2018 i686 i686 i686 GNU/Linux

--- memhack.c
+++ memhack.c
@@ -147,7 +147,8 @@ int main(int argc, char *argv[])
 	}
 	char* pid_c = NULL;
 	strcpy(pid_c, argv[2]);
-	pid = atoi(pid_c);printf("\033[42;31mline105\033[0m\n");
+printf("\033[42;31mline105\033[0m\n");
+	pid = atoi(pid_c);
 	char cmd[15];
 	init(pid_c);
 	memset(valid_addr, 0, sizeof(valid_addr));
